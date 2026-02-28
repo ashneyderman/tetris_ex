cb95c75115c00e629006c48733f2aff20f5e91e1
Alex Shneyderman <a.shneyderman@gmail.com>
2026-02-28 11:21:40 -0500

    chore: update elixir-matrix dependency to the version compatible with elixir 1.19

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Tetris.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:matrix, "~> 0.3.2"}
+      {:matrix, git: "https://github.com/ashneyderman/elixir-matrix.git", ref: "f0898b6"}
     ]
   end
 end
